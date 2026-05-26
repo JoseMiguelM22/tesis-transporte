c44843e70e48cf4f6aacbbc3fb0c294086557cc5
Jose Miguel <medinajosemiguel221@gmail.com>
2026-05-26 12:07:26 -0400

    Nuevas modificaciones

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,17 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+if (typeof window !== "undefined") {
+  // 1. Mensaje de título gigante en rojo
+  console.log(
+    "%c¡Detente!",
+    "color: red; font-family: sans-serif; font-size: 4.5em; font-weight: bold; text-shadow: 2px 2px 5px rgba(0,0,0,0.3); font-style: normal;"
+  );
+
+ 
+  console.log(
+    "%cEsta función del navegador está pensada para desarrolladores. Si alguien te indicó que copiaras y pegaras algo aquí para habilitar una función o para 'hackear' el sistema, se trata de un fraude. Si lo haces, esa persona podría acceder a tus datos o comprometer tu cuenta.",
+    "color: white; font-family: sans-serif; font-size: 1.5em; font-weight: normal; line-height: 1.4;"
+  );
+}
